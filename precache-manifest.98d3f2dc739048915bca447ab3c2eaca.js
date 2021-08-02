472a62ba8287977127ed57753f3c598c19c535b9
Abhishek Patel <Abhi882731@gmail.com>
2021-08-01 23:04:19 -0700

    Updates

---
+++ precache-manifest.98d3f2dc739048915bca447ab3c2eaca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66f5ae76851ecb96d406aa23ab8171e0",
+    "url": "/hackerNews.github.io/index.html"
+  },
+  {
+    "revision": "c014381771669fd254a5",
+    "url": "/hackerNews.github.io/static/css/main.3d77ced7.chunk.css"
+  },
+  {
+    "revision": "e970e7e7c0365f978d6a",
+    "url": "/hackerNews.github.io/static/js/2.068311d4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/hackerNews.github.io/static/js/2.068311d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c014381771669fd254a5",
+    "url": "/hackerNews.github.io/static/js/main.3835fd69.chunk.js"
+  },
+  {
+    "revision": "0fc0dfaf3e849b6318f2",
+    "url": "/hackerNews.github.io/static/js/runtime-main.8b624a75.js"
+  }
+]);
